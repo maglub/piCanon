95f455674a1267ac87165319cca7ada5d1054146
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-10-11 16:08:06 +0000

    initial commits

---
+++ setup.sh
@@ -0,0 +1,23 @@
+
+this_dir=$(cd `dirname $0`);
+. $this_dir/piCanon.conf
+
+[ -z "$configDir" ] && configDir=$this_dir/etc
+
+#=======================================
+# Install gphoto2
+#=======================================
+sudo dpkg -s ghpoto2 >/dev/null 2>&1 || { echo "  - Installing gphoto2" ; sudo apt-get -y install gphoto2 ; }
+
+#=======================================
+# Install lighttpd
+#=======================================
+
+sudo dpkg -s lighttpd >/dev/null 2>&1 || { echo "  - Installing lighttpd" ; sudo apt-get -y install lighttpd ; }
+
+[ -f /etc/lighttpd/lighttpd.conf ]  && { sudo mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.org ; sudo ln -s $configDir/lighttpd/lighttpd.conf /etc/lighttpd ; }
+[ ! -h /etc/lighttpd/conf-enabled/10-accesslog.conf ] && sudo ln -s $configDir/lighttpd/conf-enabled/10-accesslog.conf /etc/lighttpd/conf-enabled
+[ ! -h /etc/lighttpd/conf-enabled/10-dir-listing.conf ] && sudo ln -s $configDir/lighttpd/conf-enabled/10-dir-listing.conf /etc/lighttpd/conf-enabled
+[ ! -h /etc/lighttpd/conf-enabled/10-cgi.conf ] && sudo ln -s $configDir/lighttpd/conf-enabled/10-cgi.conf /etc/lighttpd/conf-enabled
+
+

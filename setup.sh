75ddefd7f9b7c15adbca29b600ca471eb085a7ff
Magnus Lubeck <magnus.lubeck@gmail.com>
2015-10-15 11:37:10 +0200

    Added sudoers file for web server permissions to run snap command

--- setup.sh
+++ setup.sh
@@ -1,5 +1,11 @@
+#!/bin/bash
 
 this_dir=$(cd `dirname $0`; pwd);
+
+echo "* Setup will check for required components and set up your raspberry pi"
+
+[ ! -f $this_dir/etc/piCanon.conf ] && { echo "Copying the etc/piCanon.conf.template to etc/piCanon.conf" ; cp $this_dir/etc/piCanon.conf.template $this_dir/etc/piCanon.conf ; }
+
 . $this_dir/etc/piCanon.conf
 
 [ -z "$configDir" ] && configDir=$this_dir/etc
@@ -8,14 +14,18 @@ this_dir=$(cd `dirname $0`; pwd);
 # Install lighttpd
 #=======================================
 
-sudo dpkg -s lighttpd >/dev/null 2>&1 || { echo "  - Installing lighttpd" ; sudo apt-get -y install lighttpd ; }
+echo "  - Checking for lighttpd"
+sudo dpkg -s lighttpd >/dev/null 2>&1 || { echo "    - Installing lighttpd" ; sudo apt-get -y install lighttpd ; }
 
-[ -f /etc/lighttpd/lighttpd.conf ]  && { sudo mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.org ; }
-[ ! -f /etc/lighttpd/lighttpd.conf ]  && { sudo ln -s $configDir/lighttpd/lighttpd.conf /etc/lighttpd ; }
-[ ! -h /etc/lighttpd/conf-enabled/10-accesslog.conf ] && sudo ln -s $configDir/lighttpd/conf-enabled/10-accesslog.conf /etc/lighttpd/conf-enabled
-[ ! -h /etc/lighttpd/conf-enabled/10-dir-listing.conf ] && sudo ln -s $configDir/lighttpd/conf-enabled/10-dir-listing.conf /etc/lighttpd/conf-enabled
-[ ! -h /etc/lighttpd/conf-enabled/10-cgi.conf ] && sudo ln -s $configDir/lighttpd/conf-enabled/10-cgi.conf /etc/lighttpd/conf-enabled
+echo "  - Setting up lighttpd configuration files"
+[ -f /etc/lighttpd/lighttpd.conf ]  && { echo "    - saving old lighttpd.conf" ; sudo mv /etc/lighttpd/lighttpd.conf /etc/lighttpd/lighttpd.conf.org ; }
+[ ! -f /etc/lighttpd/lighttpd.conf ]  && { echo "    - symlinking lighttpd.conf" ; sudo ln -s $configDir/lighttpd/lighttpd.conf /etc/lighttpd ; }
+[ ! -h /etc/lighttpd/conf-enabled/10-accesslog.conf ] && { echo "  - adding accesslog configuration" ; sudo ln -s $configDir/lighttpd/conf-enabled/10-accesslog.conf /etc/lighttpd/conf-enabled ; }
+[ ! -h /etc/lighttpd/conf-enabled/10-dir-listing.conf ] && { adding directory listing configuration" ; sudo ln -s $configDir/lighttpd/conf-enabled/10-dir-listing.conf /etc/lighttpd/conf-enabled ; }
+[ ! -h /etc/lighttpd/conf-enabled/10-cgi.conf ] && { adding cgi configuration" ; sudo ln -s $configDir/lighttpd/conf-enabled/10-cgi.conf /etc/lighttpd/conf-enabled ; }
 
+
+echo "  - Restarting lighttpd"
 sudo service lighttpd restart
 
 #=======================================
@@ -24,20 +34,29 @@ sudo service lighttpd restart
 
 for package in gphoto2 php5 php5-sqlite php5-cgi php5-cli php5-rrd perlmagick imagemagick screen
 do
-  sudo dpkg -s $package >/dev/null 2>&1 || { echo "  - Installing $package" ; sudo apt-get -y install $package; }
+  echo "  - Checking for package $package"
+  sudo dpkg -s $package >/dev/null 2>&1 || { echo "    - Installing $package" ; sudo apt-get -y install $package; }
 done
 
 #=======================================
 # Link save directory
 #=======================================
+echo "  - Setting up the save directory symlink into the html directory"
 ln -fs $baseDir/save $baseDir/html/save
 
 #=======================================
 # composer
 #=======================================
 
+echo "  - Setting up composer"
+curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
 cd $this_dir/include
-curl -s https://getcomposer.org/installer | php
-./composer.phar install
+composer install
 cd -
+
+#=======================================
+# Setup sudoers
+#=======================================
+echo "  - Setting up /etc/sudoers.d"
+sudo cp $configDir/sudoers.d/piCanon /etc/sudoers.d/piCanon ; sudo chown root:root /etc/sudoers.d/piCanon ; sudo chmod 0440 /etc/sudoers.d/piCanon 
 
